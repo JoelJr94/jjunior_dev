288373f398f24b7daf1d485ea3ad265e12672b60
JoelJr94 <joeljunior2194@gmail.com>
2023-09-27 04:45:32 -0300

    Configuracao devise, e add first_name e last_name par acriacao de usuario, atualizacao de views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user_full_name
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
+  def set_user_full_name
+    if user_signed_in?
+      @user_full_name = current_user.first_name + " " + current_user.last_name
+    end
+  end
 end
