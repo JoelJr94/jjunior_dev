f8abc73a18ce9f3c8c4985ac637d52919c872319
JoelJr94 <joeljunior2194@gmail.com>
2023-09-28 04:11:54 -0300

    Add estilos as paginas da aplicacao

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
   def render_if(condition, template, record)
     render template, record if condition
   end
+
+  def format_date(datetime)
+    datetime.strftime("%b %e, %Y")
+  end
+
+  def full_name(user)
+    (user.first_name + " " + user.last_name)
+  end
 end
