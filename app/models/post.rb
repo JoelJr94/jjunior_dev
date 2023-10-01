4494630369c38b517256aa62d88e74114ba1be25
JoelJr94 <joeljunior2194@gmail.com>
2023-10-01 01:55:40 -0300

    Criando estilos para paginas de categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  belongs_to :category
 
   validates :title, presence: true, length: {minimum: 3}
   validates :body, presence: true, length: {minimum: 10}
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_225136) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_223430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,6 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_225136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -47,5 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_225136) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
 end
