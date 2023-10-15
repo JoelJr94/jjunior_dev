a16d791b8950f1ad22233ef7459bc53110c61811
JoelJr94 <joeljunior2194@gmail.com>
2023-10-15 13:32:33 -0300

    Add e config mailcatcher

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Email
+  config.action_mailer.default_url_options = {host: "joeljrblog.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'gmail.com',
+    user_name:       Rails.application.credemtials.gmail[:user_name],
+    password:        Rails.application.credemtials.gmail[:user_password],
+    authentication:  'plain',
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 
+  }
+
 end
